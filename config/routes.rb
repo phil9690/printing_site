7956f8bfd73579ac89d17fc4f212f9942560349c
Phil <p.clauzel90@gmail.com>
2015-06-18 14:09:13 +0100

    Add static pages routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
 
+  get 'categories' => 'static_pages#categories'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
