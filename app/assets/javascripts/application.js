42b51d7305d2c6a897e8d9fe82eda7ce99d894e7
Phil <p.clauzel90@gmail.com>
2015-07-02 23:59:34 +0100

    Add slide in js effects

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,34 +18,3 @@
 
 $(function(){ $(document).foundation(); });
 
-var Slider = function() { this.initialize.apply(this, arguments) }
-Slider.prototype = {
-
-  initialize: function(slider) {
-    this.ul = slider.children[0]
-    this.li = this.ul.children
-
-    // make <ul> as large as all <li>'s
-    this.ul.style.width = (this.li[0].clientWidth * this.li.length) + 'px'
-
-    this.currentIndex = 0
-  },
-
-  goTo: function(index) {
-    // filter invalid indices
-    if (index < 0 || index > this.li.length -1)
-      return
-
-    // move <ul> left
-    this.ul.style.left = '-' + (100 * index) + '%'
-
-    this.currentIndex = index
-  },
-
-  goToNext: function() {
-    if (this.currentIndex == this.li.length -1) {
-      this.currentIndex = -1
-    }
-    this.goTo(this.currentIndex + 1)
-  }
-}
